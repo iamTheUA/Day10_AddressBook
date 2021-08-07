631b31861971c5aeed7a9b167b89ab74eebc7bfd
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-07 17:56:12 +0530

    Adding class Person

--- AddressBook.java
+++ AddressBook.java
@@ -3,8 +3,81 @@ package Day10_AddressBook;
 public class AddressBook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	}
+}
+class Person {
+	String fName;
+	String lName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	long pNumber;
+	String email;
 
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getpNumber() {
+		return pNumber;
+	}
+
+	public void setpNumber(long pNumber) {
+		this.pNumber = pNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
